f82d3edc31b3d3ff904d7c533864e9fa407c7043
Kerndog73 <kerndog73@gmail.com>
2018-09-10 14:11:48 +0930

    Setup project

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  EnTT Example
+//
+//  Created by Indi Kernick on 10/9/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#include <curses.h>
+
+int main() {
+  WINDOW *win = initsrc();
+  noecho();
+  while (true) {
+  	 
+  }
+}
