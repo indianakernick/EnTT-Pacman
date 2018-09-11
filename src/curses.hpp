9d6a9899ebceca7290a17338dd8dd211cb960bd3
Kerndog73 <kerndog73@gmail.com>
2018-09-11 16:42:17 +0930

    Build pdcurses for windows

--- src/curses.hpp
+++ src/curses.hpp
@@ -6,6 +6,13 @@
 //  Copyright © 2018 Indi Kernick. All rights reserved.
 //
 
-// Deal will cross platform stuff here
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) || defined(__CYGWIN__)
+
+#include <pdcwin.h>
+#include <pdcurses.h>
+
+#else
 
 #include <ncurses.h>
+
+#endif
