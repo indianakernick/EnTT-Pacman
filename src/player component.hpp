5f8bd49d1a1c91936ea6ee53c1cf4acfa0e7e454
Kerndog73 <kerndog73@gmail.com>
2018-09-18 19:42:35 +0930

    Add registry header

---
+++ src/player component.hpp
@@ -0,0 +1,14 @@
+//
+//  player component.hpp
+//  EnTT Example
+//
+//  Created by Indi Kernick on 18/9/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#ifndef PLAYER_COMPONENT_HPP
+#define PLAYER_COMPONENT_HPP
+
+struct Player {};
+
+#endif
