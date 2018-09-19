80b718d9cdb4e6223f8d1384b2c1ff1ed7ea693b
Kerndog73 <kerndog73@gmail.com>
2018-09-19 21:02:41 +0930

    Make vertical bounds smaller than horizontal bounds

--- src/limit movement system.cpp
+++ src/limit movement system.cpp
@@ -18,7 +18,7 @@ void limitMovement(Registry &reg) {
   	const Grid::Pos pos = view.get<Position>(e).p;
   	Grid::Dir &dir = view.get<DesiredDir>(e).d;
   	const Grid::Pos nextPos = (pos + toVec(dir)) * toVec(dir);
-  	if (std::max(nextPos.x, nextPos.y) >= 50) {
+  	if (nextPos.x >= 50 || nextPos.y >= 20) {
   	  dir = Grid::Dir::none;
   	}
   }
